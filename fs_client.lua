f9d9d781b2e03f449abc1f19e585fde962f032f7
Francuz <90420828+FrancuzDEV@users.noreply.github.com>
2022-02-26 22:04:15 +0100

    Rework, Opt

--- fs_client.lua
+++ fs_client.lua
@@ -1,4 +1,16 @@
 
+
+
+--Updating all
+-- I'm disable this for best opt
+-- Citizen.CreateThread(function()
+--     while true do
+--         ped = PlayerPedId()
+--         coords = GetEntityCoords(ped)
+--         Wait(0)
+--     end
+-- end)
+
 -- When spawn player ped --
 AddEventHandler('playerSpawned', function()
     TriggerServerEvent('FW:SpawnPlayer')
@@ -7,6 +19,7 @@ end)
 -- Setup player position --
 RegisterNetEvent('FW:lastPosition')
 AddEventHandler('FW:lastPosition', function(PosX, PosY, PosZ)
+    local ped = PlayerPedId()
     Citizen.Wait(1)
 
     local defaulModel = GetHashKey('mp_m_freemode_01')
@@ -15,18 +28,19 @@ AddEventHandler('FW:lastPosition', function(PosX, PosY, PosZ)
         Citizen.Wait(1)
     end
     SetPlayerModel(PlayerId(), defaulModel)
-    SetPedDefaultComponentVariation(PlayerPedId())
+    SetPedDefaultComponentVariation(ped)
     SetModelAsNoLongerNeeded(defaulModel)
 
-    SetEntityCoords(GetPlayerPed(-1), PosX, PosY, PosZ, 1, 0, 0, 1)
+    SetEntityCoords(ped, PosX, PosY, PosZ, 1, 0, 0, 1)
 end)
 
 -- Update player position 
 Citizen.CreateThread(function()
     while true do 
         Citizen.Wait(5000)
-
-        lastX, lastY, lastZ = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
+        local ped = PlayerPedId()
+        local coords = GetEntityCoords(ped)
+        lastX, lastY, lastZ = table.unpack(coords, true))
         TriggerServerEvent('FW:SavePlayerPosition', lastX, lastY, lastZ)
     end
 end)
